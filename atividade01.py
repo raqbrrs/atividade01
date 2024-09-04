543a34418b9b22c101c60a92961f86b0d096b435
raqbrrs <raquelbrrs07@gmail.com>
2024-09-04 14:39:15 -0300

    soma dos numeros

---
+++ atividade01.py
@@ -0,0 +1,8 @@
+# Crie um programa que receba dois números do usuário e exiba a soma desses números.
+num1 = int(input("digite o primeiro numero"))
+num2 = int(input("digite o segundo numero"))
+
+soma = num1 + num2 
+print (soma)
+
+
